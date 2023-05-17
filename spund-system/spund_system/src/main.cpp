e168a8f33ee3e1033efeeea9364f037e78c07264
rob <rob.knighton10@gmail.com>
2023-05-17 17:10:25 -0400

    d

--- spund-system/spund_system/src/main.cpp
+++ spund-system/spund_system/src/main.cpp
@@ -149,10 +149,10 @@ void publishData()
     message["memory"]["Output_memory_size"] = message.memoryUsage();
 
     // print to serial to be read by pi
-    // serializeJson(message, Serial);
+    serializeJson(message, Serial);
 
     // pretty print for debug
-    serializeJsonPretty(message, Serial);
+    // serializeJsonPretty(message, Serial);
 
     // print to serial and publish to brewblox over MQTT
     // serializeJsonPretty(message, Serial);
