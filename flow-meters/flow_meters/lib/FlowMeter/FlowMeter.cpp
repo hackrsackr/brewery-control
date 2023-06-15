a3cb823f6fc2fcf6105ab0fa40ccd421d2c9686c
rob <rob.knighton10@gmail.com>
2023-06-15 16:09:22 -0400

    deleted leftovers

--- flow-meters/flow_meters/lib/FlowMeter/FlowMeter.cpp
+++ flow-meters/flow_meters/lib/FlowMeter/FlowMeter.cpp
@@ -3,7 +3,7 @@
 #include "FlowMeter.hpp"
 
 const unsigned long MIN_INTERVAL = 1000000; // Minimum interval between flow events (in microseconds)
-// const unsigned long MIN_INTERVAL = 1000000; // Minimum interval between flow events (in microseconds)
+
 FlowMeter::FlowMeter(flowmeter_cfg_t cfg)
 {
     flowmeter_id = cfg.flow.flowmeter_id;
@@ -12,17 +12,8 @@ FlowMeter::FlowMeter(flowmeter_cfg_t cfg)
     percent_correction_factor = cfg.flow.percent_correction_factor;
 }
 
-// FlowMeter::FlowMeter(std::string id, uint8_t sp, double cf, double cr = 1)
-// {
-//     flowmeter_id = id;
-//     sensor_pin = sp;
-//     calibration_factor = cf;
-//     percent_correction_factor = cr;
-// }
-
 double FlowMeter::get_frequency()
 {
-    // frequency = (1000.0 / (millis() - old_time)) * pulse_count;
     frequency = (float(MIN_INTERVAL) / (micros() - old_time)) * pulse_count;
 
     return frequency;
@@ -34,21 +25,6 @@ double FlowMeter::get_flow_rate()
 
     return flow_rate;
 }
-
-// void FlowMeter::run()
-// {
-
-//     if ((millis() - old_time) > 1000)
-//     {
-//         detachInterrupt(sensor_pin);
-//         get_flow_rate();
-//         old_time = millis();
-//         total_pulse_count += pulse_count;
-//         total_liters = total_pulse_count / (calibration_factor * 60);
-//         total_mLs = total_liters * 1000;
-//         pulse_count = 0;
-//     }
-// }
 
 void FlowMeter::run()
 {
